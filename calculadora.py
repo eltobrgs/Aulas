0fd077f73b141f7606090a1ad4801aea9467263b
Elto Borges <eltobrgs@192.168.1.30>
2024-03-11 22:50:05 -0300

    editando calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,6 @@
-def soma(a, b):
-    return a + b
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
 
-resultado = soma(num1, num2)
+resultado = soma(n1, n2)
 print("O resultado da soma é:", resultado)
