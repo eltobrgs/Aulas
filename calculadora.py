a6782d77d0554580066b6066bc49fcd99efebb85
eltobrgs <145704868+eltobrgs@users.noreply.github.com>
2024-03-11 22:51:59 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,5 @@
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 
-resultado = soma(n1, n2)
-print("O resultado da soma é:", resultado)
+r = soma(n1, n2)
+print("O resultado da soma é:", r)
